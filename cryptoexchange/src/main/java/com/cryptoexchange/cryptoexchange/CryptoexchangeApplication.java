ebad1ba06e9a61dffc8a56c9fe4596ed31bb9227
Badegoots <garcezlucas.silva@gmail.com>
2022-10-03 15:56:36 -0300

    Iniciando o Repositório

---
+++ cryptoexchange/src/main/java/com/cryptoexchange/cryptoexchange/CryptoexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.cryptoexchange.cryptoexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoexchangeApplication.class, args);
+	}
+
+}
